97a2e1f2538a29fdc53c9a287456f264952aef66
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-10-26 10:12:26 -0600

    Routes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ import {
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Services from './pages/Services';
+import Pricing from './pages/Pricing';
+import Reviews from './pages/Reviews';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -32,7 +38,18 @@ function App() {
         <Spacer/>
           <ColorModeSwitcher justifySelf="flex-end" />
         </Flex>
-          <Home />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="services" element={<Services />} />
+          <Route exact path="pricing" element={<Pricing />} />
+          <Route exact path="reviews" element={<Reviews />} />
+          <Route exact path="about-us" element={<About />} />
+          <Route exact path="contact" element={<Contact />} />
+          {/* <Route exact path="login" element={<Login />} /> */}
+          {/* <Route exact path="new-item" element={<NewItemForm />} /> */}
+          {/* <Route path="edit-item/:itemId" element={<EditItemForm />} /> */}
+        </Routes>
+        <Home />
       </Box>
     </ChakraProvider>
   );
